bdc7a2313f843aceefc07fc8a28d720a0535e3c2
Benn McGregor <benn@bennmcgregor.com>
2026-04-26 11:06:44 -0400

    further changes to styling

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,13 +29,13 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#ffffff",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
+          darkgray: "#000000",
+          dark: "#000000",
+          secondary: "#231dc1",
+          tertiary: "#7b78ba",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.PageTitle()],
+  header: [],
   afterBody: [Component.Search()],
   footer: Component.Footer(),
 }
