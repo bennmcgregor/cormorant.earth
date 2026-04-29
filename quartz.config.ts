a6c9a13dde4c0fcc6f536401c84bfc09c65cde7c
Benn McGregor <benn@bennmcgregor.com>
2026-04-28 21:44:26 -0400

    finally got the social image rendering and made the favicon clearer

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,7 +21,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "cormorant.earth",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
